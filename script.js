37e6cd3e071847ccd2e7ee94c315179d0600fd47
bonn <116559024+bon3301@users.noreply.github.com>
2026-07-24 11:52:16 +0400

    feat: theme toggle

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+class ThemeManager {
+    constructor() {
+        this.theme = localStorage.getItem('theme') || 'dark';
+        this.button = document.getElementById('themeToggle');
+        this.icon = document.getElementById('themeIcon');
+
+        this.apply();
+        this.button.addEventListener('click', () => this.toggle());
+    }
+
+    toggle() {
+        this.theme = this.theme === 'dark' ? 'light' : 'dark';
+        this.apply();
+        localStorage.setItem('theme', this.theme);
+    }
+
+    apply() {
+        document.documentElement.setAttribute('data-theme', this.theme);
+
+        if (this.theme === 'light') {
+            this.icon.innerHTML = '';
+        } else {
+            this.icon.innerHTML = '';
+        }
+    }
+}
+
+new ThemeManager();
+
 const dropdown = document.querySelector('.dropdown');
 const dropdownToggle = document.querySelector('.dropdown-toggle');
 
