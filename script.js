68fe0e2641936e77daab94b9d5a0a242e077e969
bonn <116559024+bon3301@users.noreply.github.com>
2026-07-24 13:16:50 +0400

    dynamic background WIP

--- script.js
+++ script.js
@@ -9,6 +9,81 @@ createIcons({
     }
 });
 
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const dots = [];
+const numberOfDots = 50;
+
+for (let index = 0; index < numberOfDots; index++) {
+    dots.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        velocityX: Math.random() * 0.25 - 0.125,
+        velocityY: Math.random() * 0.25 - 0.125,
+        color: `rgb(
+            ${Math.floor(Math.random() * 256)},
+            ${Math.floor(Math.random() * 256)},
+            ${Math.floor(Math.random() * 256)}
+        )`
+    });
+}
+
+let mouseX = 0;
+let mouseY = 0;
+
+function getDistance(x1, y1, x2, y2) {
+    const xDistance = x2 - x1;
+    const yDistance = y2 - y1;
+
+    return Math.sqrt(
+        Math.pow(xDistance, 2) + Math.pow(yDistance, 2)
+    );
+}
+
+function drawDots() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let index = 0; index < numberOfDots; index++) {
+        const dot = dots[index];
+
+        if (getDistance(dot.x, dot.y, mouseX, mouseY) < 10) {
+            continue;
+        }
+
+        context.beginPath();
+        context.arc(dot.x, dot.y, 3, 0, Math.PI * 2);
+        context.fillStyle = dot.color;
+        context.fill();
+
+
+        if (dot.x < 0 || dot.x > canvas.width) {
+            dot.velocityX *= -1;
+        }
+
+        if (dot.y < 0 || dot.y > canvas.height) {
+            dot.velocityY *= -1;
+        }
+    }
+
+    requestAnimationFrame(drawDots);
+}
+
+drawDots();
+
+canvas.addEventListener('mousemove', (event) => {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+});
+
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
 class ThemeManager {
     constructor() {
         this.theme = localStorage.getItem('theme') || 'dark';
